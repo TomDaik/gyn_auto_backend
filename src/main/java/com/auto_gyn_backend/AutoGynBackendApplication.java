7d0ceedd4b0237b2a51d8cb9aff9e59606b711f3
Pedro Queiroz <pqlb1512@gmail.com>
2025-06-03 20:37:59 -0300

    criação do repositorio

---
+++ src/main/java/com/auto_gyn_backend/AutoGynBackendApplication.java
@@ -0,0 +1,13 @@
+package com.auto_gyn_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGynBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoGynBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/auto_gyn_backend/AutoGynBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auto_gyn_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoGynBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
